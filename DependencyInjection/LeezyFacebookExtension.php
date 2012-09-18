ab876f1646fbdadabfb5be9b3eb3034d07382b4d
Thomas Tourlourat <thomas@tourlourat.com>
2012-09-18 17:17:41 +0200

    Remove Leezy.

--- DependencyInjection/LeezyFacebookExtension.php
+++ DependencyInjection/LeezyFacebookExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class LeezyFacebookExtension extends Extension
+class ArmetizFacebookExtension extends Extension
 {
     /**
      * {@inheritDoc}
